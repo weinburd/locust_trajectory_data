4d5e37759246ab6034824592f8e0ebeb99b29332
Jasper Weinburd <jweinburd@hmc.edu>
2023-11-26 14:39:12 -0600

    Cosmetic changes

--- Figures/fig1_traj.m
+++ Figures/fig1_traj.m
@@ -26,11 +26,11 @@ file_idx = 1;
 clipNum = 7; %can change to 9
 
 %%% Options %%%
-saveFigs = 0;
+saveFigs = 1;
 % assembleData = 0; %and save it to figDataFile
     %else loads data from figDataFile
 %%%
-vidFlag = 0; %whether or not to load data from the video clip
+vidFlag = 1; %whether or not to load data from the video clip
 
 % Unpack the data and metadata
 vidName = recording(file_idx).data{clipNum,1};
@@ -40,7 +40,7 @@ data_struct = recording(file_idx).data{clipNum,2};
 [data_final, ~] = struct2data(data_struct);
 
 if vidFlag
-    v = ['preprocessed_' clipName '.avi'];
+    v = ['preprocessed_' clipName(1:end-4) '.avi']; %seem to need to remove the final "_720"
     % The video file is incompatible with MacOS
     % See an explanation here: 
     % https://www.mathworks.com/matlabcentral/answers/395560-why-does-videoreader-not-load-a-video-in-r2017a-or-later-that-used-to-load-in-r2016b-on-mac
@@ -64,6 +64,8 @@ figWidth = 16;
 titleSize = 20; %fontsize
 labelSize = 24;
 ticklabelSize = 18;
+trail_width = 5; % sample trajectory widths
+alpha = .4; % sample trajectory transparency
 
 %% Set up a figure for plotting
 figNum = 1011;
@@ -76,36 +78,6 @@ set(h,'PaperPosition',[ 0 0 figWidth figWidth/wid*hei]);
 set(h,'Position',[ 0 0 figWidth figWidth/wid*hei]);
 movegui(h,'north')
 
-%title(['Time ',num2str(0,'%3.1f'), ' (sec)'],'FontSize',titleSize);
-xlabel('$x$ (centimeters)','FontSize',labelSize)
-ylabel('$y$ (centimeters)','FontSize',labelSize)
-
-scalefactor = sqrt(wid*hei)*0.02*scale;
-imageAxis = [fieldDims(1:2) fieldDims(3) fieldDims(4)]*scale;
-%[fieldDims(1:2) -fieldDims(4) -fieldDims(3)]*scale;
-axis(imageAxis)
-ax = gca;
-
-ax.FontSize = ticklabelSize;
-xtlab = linspace(0,100,11);
-ytlab = linspace(0,50,6);
-xticks(xtlab*scale)
-yticks(ytlab*scale)
-xticklabels(cellfun(@num2str,num2cell(xtlab),'un',0))
-yticklabels(cellfun(@num2str,num2cell(ytlab),'un',0))
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Touch Up %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %remove whitespace
-    ax = gca;
-    outerpos = ax.OuterPosition;
-    ti = ax.TightInset; 
-    left = outerpos(1) + ti(1);
-    bottom = outerpos(2) + ti(2);
-    ax_width = outerpos(3) - ti(1) - ti(3);
-    ax_height = outerpos(4) - ti(2) - ti(4) - .015;
-    ax.Position = [left bottom ax_width ax_height];
 
 pbaspect([wid,hei,1]);
 % colors
@@ -176,27 +148,72 @@ for ii = loc_idx
     
     for jj = stop_steps
         step = jj-1:jj;
-        plot(x_line(ii, step), y_line(ii, step), "color", colors(7,:), 'LineWidth', 1.5 )
+        plot(x_line(ii, step), y_line(ii, step), "color", [colors(7,:) alpha], 'LineWidth', trail_width )
     end
     for jj = crawl_steps
         step = jj-1:jj;
-        plot(x_line(ii, step), y_line(ii, step), "color", yellow, 'LineWidth', 1.5 )
+        plot(x_line(ii, step), y_line(ii, step), "color", [yellow alpha], 'LineWidth', trail_width)
     end
     for jj = hop_steps
         step = jj-1:jj;
-        plot(x_line(ii, step), y_line(ii, step), "color", blueishgreen, 'LineWidth', 1.5 )
+        plot(x_line(ii, step), y_line(ii, step), "color", [blueishgreen alpha], 'LineWidth', trail_width)
     end
 end
 
-myQuiv([true;false;true], blueishgreen, x_line(loc_filt,T_end), y_line(loc_filt,T_end), theta_line(loc_filt,T_end), scalefactor)
-myQuiv([false;true;false], yellow, x_line(loc_filt,T_end), y_line(loc_filt,T_end), theta_line(loc_filt,T_end), scalefactor)
+%myQuiv([true;false;true], blueishgreen, x_line(loc_filt,T_end), y_line(loc_filt,T_end), theta_line(loc_filt,T_end), scalefactor)
+%myQuiv([false;true;false], yellow, x_line(loc_filt,T_end), y_line(loc_filt,T_end), theta_line(loc_filt,T_end), scalefactor)
 
-myQuiv(idx_stop(:,T_end), colors(7,:), x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor)
-myQuiv(idx_crawl(:,T_end), yellow, x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor)
-myQuiv(idx_hop(:,T_end), blueishgreen, x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor)
+% a non-existent scatter plot to create the legend.
+% try as I might I could not make the little circles bigger!
+q_legend = { scatter(nan, nan, 'MarkerEdgeColor', colors(7,:), "LineWidth", 1.5), ...
+             scatter(nan, nan, 'MarkerEdgeColor', yellow, "LineWidth", 1.5), ...
+             scatter(nan, nan, 'MarkerEdgeColor', blueishgreen, "LineWidth", 1.5)};
+
+qs = myQuiv(idx_stop(:,T_end), colors(7,:), x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor);
+qc = myQuiv(idx_crawl(:,T_end), yellow, x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor);
+qh = myQuiv(idx_hop(:,T_end), blueishgreen, x_line(:,T_end), y_line(:,T_end), theta_line(:,T_end), scalefactor);
 
 %% add a legend?
 %TODO
+l = legend([q_legend{:}], ...
+       {"Stationary Locust", "Walking Locust", "Hopping Locust"},...
+       "Location", "SouthEast", ...
+       "FontSize", ticklabelSize+2);
+
+%% Axes and stuff
+
+scalefactor = sqrt(wid*hei)*0.02*scale;
+imageAxis = [fieldDims(1:2) fieldDims(3) fieldDims(4)]*scale;
+%[fieldDims(1:2) -fieldDims(4) -fieldDims(3)]*scale;
+axis(imageAxis)
+ax = gca;
+
+ax.FontSize = ticklabelSize;
+xtlab = linspace(0,100,11);
+ytlab = linspace(0,50,6);
+xticks(xtlab*scale)
+yticks(ytlab*scale)
+xticklabels(cellfun(@num2str,num2cell(xtlab),'un',0))
+yticklabels(cellfun(@num2str,num2cell(ytlab),'un',0))
+
+%title(['Time ',num2str(0,'%3.1f'), ' (sec)'],'FontSize',titleSize);
+title('Single Video Frame with Tracked Positions and Sample Trajectories', 'FontSize',labelSize+2)
+xlabel('$x$ (centimeters)','FontSize',ticklabelSize+2)
+ylabel('$y$ (centimeters)','FontSize',ticklabelSize+2)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Touch Up %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %remove whitespace
+    ax = gca;
+    outerpos = ax.OuterPosition;
+    ti = ax.TightInset; 
+    left = outerpos(1) + ti(1);
+    bottom = outerpos(2) + ti(2);
+    ax_width = outerpos(3) - ti(1) - ti(3);
+    ax_height = outerpos(4) - ti(2) - ti(4) - .015;
+    ax.Position = [left bottom ax_width ax_height];
+
 
 %% save figure and data
 %add assemble data functionality?
@@ -211,9 +228,11 @@ end
 
 %% Associated Functions %%
 
-    function myQuiv(idx,color, xposn, yposn, theta, scalefactor)
-        quiver(xposn(idx),yposn(idx),scalefactor*cos(theta(idx)),scalefactor*sin(theta(idx)),...
+    function q = myQuiv(idx,color, xposn, yposn, theta, scalefactor)
+        q = quiver(xposn(idx),yposn(idx),scalefactor*cos(theta(idx)),scalefactor*sin(theta(idx)),...
                'o','MarkerSize', 14,'Color',color,'AutoScale','off', 'LineWidth', 1.5);
+        q.ShowArrowHead = 'off';
+        q.MaxHeadSize = 0;
     end
 
 %end
